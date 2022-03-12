68e9d426e23762e92b88410aee25fa061a15da1a
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-03-12 12:49:44 -0800

    workshop 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
+$(function () {
+    $(".carousel").carousel({
+        interval: 2000
+    });
+    $("#carouselButton").click(function () {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
@@ -8,7 +10,13 @@ $(function() {
         } else {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
+            $("#carouselButton").children("i").addClass("fa-pause");
         }
+    });
+    $("#reserveButton").on("click", function () {
+        $("#reserveModal").modal('show');
+    });
+    $("#loginButton").on("click", function () {
+        $("#loginModal").modal('show');
     });
 });
